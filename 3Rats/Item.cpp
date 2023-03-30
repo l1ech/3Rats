79105dc21538ed5b7adac84094c1ccf1fdb518f8
Lyka:11 <Lukdz@yahoo.de>
2023-03-30 14:52:52 +0200

    removed unneded constructor for item

--- 3Rats/Item.cpp
+++ 3Rats/Item.cpp
@@ -1,133 +1,15 @@
 #include "Item.h"
 
 Item::Item()
-{	file_path = "meta_textures/place_holder.png";
-
-	is_on_map = false;
-	is_picked_up = false;
-
-	//std::cout << "const" << std::endl;
-}
-
-Item::Item(SDL_Renderer* renderTarget, std::string filePath, int x, int y)
 {
 	is_on_map = false;
 	is_picked_up = false;
-
-	SDL_Surface* surface = IMG_Load(filePath.c_str());
-	if (surface == NULL)
-		std::cout << "Error Item Surface" << std::endl;
-	else
-	{
-		texture = SDL_CreateTextureFromSurface(renderTarget, surface);
-		if (texture == NULL)
-			std::cout << "Error Item Texture" << std::endl;
-	}
-
-	SDL_FreeSurface(surface);
-
-	SDL_QueryTexture(texture, NULL, NULL, &crop_rect.w, &crop_rect.h);
-
-	position_rect.x = x;
-	position_rect.y = y;
-
-	texture_width = crop_rect.w;
-
-	frame_width = position_rect.w = crop_rect.w;
-	frame_height = position_rect.h = crop_rect.h;
-
-	origin_x = frame_width / 2;
-	origin_y = frame_height / 2;
-
-	radius = frame_width / 2;
-
-	//isActive = false;
-}
-
-Item::Item(Item& other)
-{
-	//std::cout << "copy" << std::endl;
-
-	is_on_map = other.is_on_map;
-	is_picked_up = other.is_picked_up;
-
-	file_path = other.file_path;
-	ptr_renderer = other.ptr_renderer;
-
-	SDL_Surface* surface = IMG_Load(file_path.c_str());
-	if (surface == NULL)
-		std::cout << "Error Item Surface" << std::endl;
-	else
-	{
-		texture = SDL_CreateTextureFromSurface(ptr_renderer, surface);
-		if (texture == NULL)
-			std::cout << "Error Item Texture" << std::endl;
-	}
-
-	SDL_FreeSurface(surface);
-
-	SDL_QueryTexture(texture, NULL, NULL, &crop_rect.w, &crop_rect.h);
-
-	position_rect.x = other.position_rect.x;
-	position_rect.y = other.position_rect.y;
-
-	texture_width = crop_rect.w;
-
-	frame_width = position_rect.w = crop_rect.w;
-	frame_height = position_rect.h = crop_rect.h;
-
-	origin_x = frame_width / 2;
-	origin_y = frame_height / 2;
-
-	radius = frame_width / 2;
 }
 
 Item::~Item()
 {
 	SDL_DestroyTexture(texture);
 }
-/*
-Item& Item::operator=(Item& other)
-{
-	std::cout << "=" << std::endl;
-
-	is_on_map = other.get_on_map();
-	is_picked_up = other.get_pick_up();
-
-	filePath = other.filePath;
-	ptr_renderer = other.ptr_renderer;
-
-	SDL_Surface* surface = IMG_Load(filePath.c_str());
-	if (surface == NULL)
-		std::cout << "Error Item Surface" << std::endl;
-	else
-	{
-		texture = SDL_CreateTextureFromSurface(ptr_renderer, surface);
-		if (texture == NULL)
-			std::cout << "Error Item Texture" << std::endl;
-	}
-
-	SDL_FreeSurface(surface);
-
-	SDL_QueryTexture(texture, NULL, NULL, &crop_rect.w, &crop_rect.h);
-
-	position_rect.x = other.position_rect.x;
-	position_rect.y = other.position_rect.y;
-
-	textureWidth = crop_rect.w;
-
-	frameWidth = position_rect.w = crop_rect.w;
-	frameHeight = position_rect.h = crop_rect.h;
-
-	originX = frameWidth / 2;
-	originY = frameHeight / 2;
-
-	radius = frameWidth / 2;
-
-	return *this;
-}
-
-*/
 	
 //void Item::Update(float delta) { }
 
--- 3Rats/Item.h
+++ 3Rats/Item.h
@@ -23,12 +23,8 @@ private:
 
 
 public:
-	Item(SDL_Renderer* renderTarget, std::string filePath, int x, int y);
-	Item(Item& other);
 	Item();
 	~Item();
-
-	//Item& operator=(Item& other);
 
 	//void Update(float delta);
 	void Draw(SDL_Renderer* renderTarget);
