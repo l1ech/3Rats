83c4ed703412c767705d1937cc5a2d4cf8f0f631
Lyka:11 <Lukdz@yahoo.de>
2023-03-31 18:17:19 +0200

    seed implemented

--- 3Rats/Random.h
+++ 3Rats/Random.h
@@ -4,7 +4,14 @@
 
 class Random
 {
+private:
+	typedef std::mt19937 MyRNG;  // the Mersenne Twister with a popular choice of parameters
+	uint32_t seed_val;           // populate somehow
+	MyRNG rng;                   // e.g. keep one global instance (per thread)
 public:
+
+	Random(uint32_t seed_val);
+
 	bool flip_coin();
 	int roll_dice();
 	int roll_custom_dice(int num);
